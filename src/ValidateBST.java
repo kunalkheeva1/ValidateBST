f4650a6f6a4d18b1e2b4b261cea62c8f1c31d48a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 19:40:35 -0700

    coded a node class

--- src/ValidateBST.java
+++ src/ValidateBST.java
@@ -1,5 +1,15 @@
 public class ValidateBST {
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val, Node left, Node right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 }
