bc22a194b53653be71d487c060957ec5b70d1850
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 19:52:47 -0700

    added comments for the better understanding

--- src/ValidateBST.java
+++ src/ValidateBST.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class ValidateBST {
+    //tree node
     class Node{
         int val;
         Node left;
@@ -11,15 +12,19 @@ public class ValidateBST {
             this.right = right;
         }
     }
+    //inorder, so that every element sits in ascending order
     static void inoder(Node node, ArrayList<Integer> ans){
         if(node == null) return;
         inoder(node.left,ans);
         ans.add(node.val);
         inoder(node.right,ans);
     }
+    // validate it by checking the next element with previous, if previous is smaller than return false
     static boolean validateBST(Node root){
         ArrayList<Integer> ans = new ArrayList<>();
         inoder(root, ans);
+        //starting the index from 1, because it needs to be compared with previous (0 here)
+        // if i take 0 index then i need to compare it with -1, which contains no element.
         for(int i= 1; i<ans.size(); i++){
             if(ans.get(i)<= ans.get(i-1)){
                 return false;
